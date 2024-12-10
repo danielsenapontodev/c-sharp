1013ba6d39a1120a5a9acf16547e7b43058076cf
Daniel Sena <danielsena.dev@outlook.com>
2024-12-09 23:11:06 -0300

    Cap. 4 (wip)

---
+++ Cap04/ModelProject/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ModelProject
+{
+    class Fornecedor
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
